fe0f619db9a8658266def3bee0eae8b1b6212415
OloioiOl <chansik4294@cau.ac.kr>
2026-08-27 16:04:29 +0900

    강의 중 ab setting

--- src/test/java/myspring/di/xml/HelloBeanJunitTest.java
+++ src/test/java/myspring/di/xml/HelloBeanJunitTest.java
@@ -1,5 +1,6 @@
 package myspring.di.xml;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -20,7 +21,20 @@ public class HelloBeanJunitTest {
 	@Test
 	void helloBeanSetter() {
 		System.out.println("==> helloBeanSetter");
+		Hello hello1 = (Hello)context.getBean("hello");
+		Hello hello2 = context.getBean("hello",Hello.class);
 		
+		//주소 비교
+		System.out.println(hello1 == hello2);
+		assertSame(hello1, hello2);
+		
+		//값 비교
+		assertEquals("Hello 스프링", hello1.sayHello());
+		
+		hello1.print();
+		
+		Printer printer = context.getBean("strPrinter",Printer.class);
+		assertEquals("Hello 스프링", printer.toString());
 	}
 	
 	
